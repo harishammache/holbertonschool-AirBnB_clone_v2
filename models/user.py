d0e4e03aaa2492c179a013669732feea592cd0f4
harishammache <hammache_haris@hotmail.fr>
2024-03-26 10:12:41 +0000

    add a def init

--- models/user.py
+++ models/user.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+import sqlalchemy
 from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
@@ -12,3 +15,7 @@ class User(BaseModel):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """inherits for basemodel"""
+        super().__init__(*args, **kwargs)
