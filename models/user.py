f644a5135a81654feefae14bb8aa2b15487f792f
harishammache <hammache_haris@hotmail.fr>
2024-03-26 11:16:18 +0000

    I restored the file to its original state" in English.

--- models/user.py
+++ models/user.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-import models
 from models.base_model import BaseModel
-from models.base_model import Base
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 
-class User(BaseModel, Base):
-    """update user inherits for BaseModel"""
-    __tablename__ = "users"
-
-    id = Column(String(60), primary_key=True)
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-
-    def __init__(self, *args, **kwargs):
-        """inherit to Base and Base_model"""
-        super().__init__(*args, **kwargs)
+class User(BaseModel):
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
