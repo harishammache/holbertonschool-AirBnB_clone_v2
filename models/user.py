4103ed76d54ed81ad8731bf09b5bc4f55820350e
harishammache <hammache_haris@hotmail.fr>
2024-03-26 10:09:20 +0000

    change the value of string

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ class User(BaseModel):
     """update user inherits for BaseModel"""
     __tablename__ = "users"
 
-    email = Column(String(127), nullable=False)
+    email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
