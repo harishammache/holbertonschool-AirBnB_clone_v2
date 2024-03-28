c5f0ad4d386bc87da2e3bf1f8bd3ff3ea6ae0a70
harishammache <hammache_haris@hotmail.fr>
2024-03-28 11:37:36 +0000

    delete to delete-orphan

--- models/user.py
+++ models/user.py
@@ -13,6 +13,6 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref="user",
-                          cascade="all, delete")
+                          cascade="all, delete-oprhan")
     reviews = relationship("Review", backref="user",
-                           cascade="all, delete")
+                           cascade="all, delete-orphan")
