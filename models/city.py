2e455151e870b100d8bf19dd4cb029dbdf49d9c2
harishammache <hammache_haris@hotmail.fr>
2024-03-28 11:29:38 +0000

    change dekete to delete-orphan

--- models/city.py
+++ models/city.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -13,4 +12,4 @@ class City(BaseModel, Base):
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", backref="cities",
-                          cascade="all, delete")
+                          cascade="all, delete-orphan")
