3c887fd178c59ced73fa5379043ffd50bf926d71
harishammache <hammache_haris@hotmail.fr>
2024-03-28 11:27:38 +0000

    change syntax

--- models/amenity.py
+++ models/amenity.py
@@ -9,6 +9,10 @@ import os
 class Amenity(BaseModel, Base):
     """inheritated class Amenity from BaseModel"""
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    from models.place import place_amenity
-    place_amenities = relationship("Place", secondary=place_amenity)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        from models.place import place_amenity
+        place_amenities = relationship(
+            "Place", secondary="place_amenity")
+    else:
+        name = ""
