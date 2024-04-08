f5874ee3d47229883cb794c0e80232865c92e6d3
harishammache <hammache_haris@hotmail.fr>
2024-04-08 15:09:50 +0000

    upgrade

--- models/city.py
+++ models/city.py
@@ -7,20 +7,23 @@ from os import getenv
 
 
 class City(BaseModel, Base):
-    """Class to declare the cities database table
-    """
-    storage_engine = getenv('HBNB_TYPE_STORAGE')
-    if storage_engine is None:
-        storage_engine = "db"
+    """This class defines a city by various attributes."""
+    __tablename__ = 'cities'
 
-    if storage_engine == "db":
-        __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities",
+                          cascade="all, delete-orphan")
 
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        state = relationship("State", back_populates="cities")
-        places = relationship("Place", back_populates="cities",
-                              cascade="delete, delete-orphan")
-    else:
-        name = ""
-        state_id = ""
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Retourne la liste des objets City liés à l'État courant"""
+            from models import storage
+            from models.city import City
+
+            cities_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
