4c942af98f0f1b133f2abd12fed2e47bdb53000e
harishammache <hammache_haris@hotmail.fr>
2024-04-08 11:36:54 +0000

    add defaults value for text

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,7 +22,7 @@ def c(text):
     return 'C ' + text.replace('_' + ' ')
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is_cool'):
     """display /python/<text>"""
