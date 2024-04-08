402711ac9f24aaabd1b7db0177fcb4af63da3e07
harishammache <hammache_haris@hotmail.fr>
2024-04-08 11:40:18 +0000

    add defaultd value

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,9 +22,9 @@ def c(text):
     return 'C ' + text.replace('_' + ' ')
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text='is_cool'):
+def python(text):
     """display /python/<text>"""
     return 'Python ' + text.replace('_' + ' ')
 
