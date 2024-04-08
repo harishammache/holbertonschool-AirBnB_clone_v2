ae67fe180791ac6ee517bb43d9d0ea4777f17ab8
harishammache <hammache_haris@hotmail.fr>
2024-04-08 15:07:42 +0000

    upgrade

--- models/city.py
+++ models/city.py
@@ -6,20 +6,21 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-class State(BaseModel, Base):
-    __tablename__ = 'states'
+class City(BaseModel, Base):
+    """Class to declare the cities database table
+    """
+    storage_engine = getenv('HBNB_TYPE_STORAGE')
+    if storage_engine is None:
+        storage_engine = "db"
 
-    name = Column(String(128), nullable=False)
+    if storage_engine == "db":
+        __tablename__ = 'cities'
 
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """Retourne la liste des objets City liés à l'État courant"""
-            from models import storage
-            from models.city import City
-
-            cities_list = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    cities_list.append(city)
-            return cities_list
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        state = relationship("State", back_populates="cities")
+        places = relationship("Place", back_populates="cities",
+                              cascade="delete, delete-orphan")
+    else:
+        name = ""
+        state_id = ""
