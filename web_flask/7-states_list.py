9d157752c9735456403e28a76375fb28b51d8239
harishammache <hammache_haris@hotmail.fr>
2024-04-12 07:54:37 +0000

    change "states" to State

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,13 +2,14 @@
 """script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display a HTML page"""
-    states = storage.all()
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
