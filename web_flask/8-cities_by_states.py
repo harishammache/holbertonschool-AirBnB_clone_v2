d502848175320563849a5735189aa99dd01772c0
harishammache <hammache_haris@hotmail.fr>
2024-04-12 08:23:48 +0000

    change order

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+"""
+Module for start a web aplication via Flask
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page"""
-    states = storage.all(State)
-    return render_template("8-cities_by_states.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown():
-    """remove the current SQLAlchemy Session"""
+def close(self):
+    """Number_template/<n> page"""
     storage.close()
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display States"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
