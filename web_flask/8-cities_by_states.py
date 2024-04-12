e41c030a97afc01a60b5fc50f8a7bb9eb0e1e958
harishammache <hammache_haris@hotmail.fr>
2024-04-12 09:41:23 +0000

    change name of function

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,8 +12,8 @@ def close(self):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
     """display a HTML page"""
     states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
