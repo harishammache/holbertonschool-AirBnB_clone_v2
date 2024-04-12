96ec6300217d6fc2c5d26bfbf9ecafc1a9ddcd32
harishammache <hammache_haris@hotmail.fr>
2024-04-12 11:04:33 +0000

    upgrade

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+"""
+Module for start a web aplication via Flask
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,13 +10,13 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close(self):
-    """remove the current SQLAlchemy Session"""
+    """Number_template/<n> page"""
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """display a HTML page"""
+    """Display States"""
     states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 
